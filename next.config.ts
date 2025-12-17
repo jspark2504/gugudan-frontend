bd19edff7d0e4f52be05d3d70238be36057056cd
keaunsol Na <knsol1992@naver.com>
2025-12-18 00:52:05 +0900

    fix: [나큰솔] CloudFront 캐싱 관련

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+        ],
+    },
+
+    async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "no-store",
+                    },
+                ],
+            },
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "no-store",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
